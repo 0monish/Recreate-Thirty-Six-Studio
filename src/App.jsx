4bbccd75bc81126905312f9c2adbb2d8bcd55e7c
0Monish <0monishkhandelwal@gmail.com>
2025-02-17 23:25:05 +0530

    Worked on animations

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,38 @@ const App = () => {
   const growingSpan = useRef(null)
   const curvedText = useRef(null)
 
+  const text = "We provide you with captivating design, interactive animations, reliable code, and immaculate project coordination. Whether you need a campaign built from scratch or assistance at a specific phase, we've got you covered.";
+
+  const newText = text.split(" ").map((word, wordIndex) => {
+    const letters = [...word].map((char, charIndex) => <span key={charIndex}>{char}</span>);
+    return (
+      <span key={wordIndex}>
+        {letters}
+        {wordIndex < text.split(" ").length - 1 && <span>&nbsp;</span>}
+      </span>
+    );
+  });
+
   useEffect(() => {
-   
+
     if (curvedText.current) {
       new CircleType(curvedText.current);
     }
   }, []);
 
   useGSAP(() => {
+
+    // NAVIGATION SECTION
+    gsap.from("nav a", {
+      y: 200,
+      delay: 0,
+      opacity: 0,
+      duration: 2,
+      stagger: 0.2,
+      ease: "power2.out",
+    })
+
+    // HEADING SECTION
     gsap.from("h1 span", {
       y: 200,
       delay: 1,
@@ -34,19 +58,59 @@ const App = () => {
       ease: "elastic.out(0.5,0.5)",
     });
 
-    gsap.from("nav a", {
-      y: 200,
-      delay: 0,
+    // WHAT WE DO SECTION
+    gsap.from("h3", {
+      y: 20,
       opacity: 0,
-      duration: 2,
-      stagger: 0.2,
-      ease: "power2.out",
+      stagger: 2,
+      duration: 8,
+      ease: "elastic.out(0.5,0.5)",
+      scrollTrigger: {
+        trigger: "h3",
+        scrub: 1,
+        start: "50% 50%",
+        end: "50% 20%",
+        // markers: true
+      }
+    })
+
+
+    // SERVICES SECTION
+    gsap.from("#services", {
+      y: 50,
+      opacity: 0,
+      duration: 1,
+      delay: 1,
+      ease: "elastic.out(0.2,0.5)",
+      scrollTrigger: {
+        trigger: "#services",
+        scrub: 1,
+        start: "top 50%",
+        end: "top 20%",
+        // markers: true
+      }
+    })
+
+    gsap.from("h2 span", {
+      y: 20,
+      opacity: 0,
+      stagger: 1,
+      rotate: 10,
+      duration: 10,
+      ease: "elastic.out(0.2,0.5)",
+      scrollTrigger: {
+        trigger: "h2",
+        scrub: 1,
+        start: "top 50%",
+        end: "40% 50%",
+        // markers: true
+      }
     })
 
     const animation = gsap.to("#curved-text", {
       delay: 1,
       rotate: 360,
-      duration: 20, // Default duration
+      duration: 20,
       repeat: -1,
       ease: "linear"
     });
@@ -115,7 +179,6 @@ const App = () => {
         </button>
       </nav>
 
-      {/* Hero Section */}
       <main className="flex-1 flex flex-col mt-5 ">
         <div className="w-full flex flex-col md:flex-row items-center">
 
@@ -148,15 +211,69 @@ const App = () => {
       </main>
 
 
-      <div className="w-full">
-        <h1 className="text-[6rem] md:text-[11.5rem] mx-5">
-          <span>Thirty</span><span>Six</span><span>Studios</span>
-        </h1>
+      <div className="w-full min-h-screen ">
+
+        {/* HEADING SECTION */}
+        <div>
+          <h1 className="text-[6rem] md:text-[11.5rem] mx-5">
+            <span>
+              <span>T</span><span>h</span><span>i</span><span>r</span><span>t</span><span>y</span>
+            </span>
+            <span>
+              <span>S</span><span>i</span><span>x</span>
+            </span>
+            <span>
+              <span>S</span><span>t</span><span>u</span><span>d</span><span>i</span><span>o</span><span>s</span>
+            </span>
+          </h1>
+        </div>
+
+        <div className="w-full flex flex-col md:flex-row mt-40 space-y-8">
+          <div className="max-w-3xl mx-5 md:ml-[25%] md:mr-[21%]">
+            <h3 className="text-2xl">
+              1 - What We Do
+            </h3>
+          </div>
+
+          <div className="md:max-w-xs mx-5 md:mr-[10%] space-y-8">
+            <h3 className="text-3xl ">
+              We aim to elevate digital production in the advertising space, bringing your ideas to life.
+            </h3>
+
+            <h3 className="mt-30">As a contemporary studio, we use cutting-edge design practices and the latest technologies to deliver current digital work.
+
+            </h3>
+            <h3>Our commitment to innovation and simplicity, paired with our agile approach, ensures your journey with us is smooth and enjoyable from start to finish.</h3>
+          </div>
+        </div>
       </div>
+
+
+      {/* OUR SERVICES SECTION */}
+      <div className="w-full min-h-screen ">
+        <div className="w-full flex flex-col mt-40 space-y-8">
+          <div className="mx-5 md:ml-[25%] md:mr-[21%]">
+            <h2 id="services" className="text-2xl">
+              2 - Our Services
+            </h2>
+          </div>
+
+          <div >
+            <h2 className="text-4xl mx-5 md:ml-[25%] md:mr-[15%] text-wrap">
+              {newText}
+            </h2>
+
+          </div>
+        </div>
+      </div>
+
+      {/* <div className='w-full min-h-screen border-1'>
+
+      </div> */}
 
 
     </div>
   );
 }
 
-export default App  
+export default App
